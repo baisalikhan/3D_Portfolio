2c24bd949eb5de8fb4609e16e83b1063e3aad72e
baisexodevs <112929397+baisalikhan@users.noreply.github.com>
2025-04-18 23:37:37 +0500

    fix: completed the Navbar, logoShowCase and FeatureCards

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import ShowCaseSection from "./sections/ShowcaseSection";
+import NavBar from "./components/NavBar";
+import LogoShowcase from "./sections/LogoShowcase";
+import FeatureCards from "./sections/FeatureCards";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowCaseSection />
+      <LogoShowcase />
+      <FeatureCards />
     </>
   );
 };
