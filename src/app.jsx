286da44424b59fd5eb1b3406d7f3e001cbf7a3bf
baisexodevs <112929397+baisalikhan@users.noreply.github.com>
2025-04-19 00:00:05 +0500

    feat: completed the experienceSection

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import ShowCaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
 import LogoShowcase from "./sections/LogoShowcase";
 import FeatureCards from "./sections/FeatureCards";
+import Experience from "./sections/Experience";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <ShowCaseSection />
       <LogoShowcase />
       <FeatureCards />
+      <Experience />
     </>
   );
 };
